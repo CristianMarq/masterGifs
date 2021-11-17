4e3586bb1ed578072a29974801c5a2e0c5717926
Christian Marquez <marquezcristian920@gmail.com>
2021-11-16 21:33:40 -0700

    Megorando estructura de directorios

--- src/GifGrid.js
+++ src/GifGrid.js
@@ -1,40 +1,22 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
 import { GifGriditem } from "./GifGriditem";
+import { getGif } from "./helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
   const [images, setimages] = useState([]);
 
   useEffect(() => {
-    getGif();
-  }, []);
-
-  const getGif = async () => {
-    const url =
-      "https://api.giphy.com/v1/gifs/search?q=Dragon Ball Super&limit=10&api_key=PpE7mVViDjMLYfStwlEziP4HMUDIMOlz";
-
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-
-    console.log(gifs);
-    setimages(gifs);
-  };
+    getGif(category).then((cat) => setimages(cat));
+  }, [category]);
 
   return (
-    <div>
-      <h3>{category}</h3>
-
-      {images.map((img) => {
-        return <GifGriditem key={img.id} {...img} />;
-      })}
-    </div>
+    <>
+      <h3>{category}</h3>{" "}
+      <div className="card-grid">
+        {images.map((img) => {
+          return <GifGriditem key={img.id} {...img} />;
+        })}
+      </div>
+    </>
   );
 };
